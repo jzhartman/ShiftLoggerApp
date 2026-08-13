208811164f034186c8e1dda6cc8be3aa3f4e6778
jzhartman <jzhartman@gmail.com>
2026-08-12 22:58:39 -0400

    GetAllShiftsByEmployeeId implements results pattern

--- ShiftLogger.Application/Shifts/Requests/GetShiftsByEmployeeId/GetShiftsByEmployeeIdHandler.cs
+++ ShiftLogger.Application/Shifts/Requests/GetShiftsByEmployeeId/GetShiftsByEmployeeIdHandler.cs
@@ -1,5 +1,6 @@
 ﻿using ShiftLogger.Application.Shifts.Dtos;
 using ShiftLogger.Domain.Models;
+using ShiftLogger.Domain.Validation;
 using ShiftLogger.Infrastructure.Repositories;
 
 namespace ShiftLogger.Application.Shifts.Requests.GetShiftsByEmployeeId;
@@ -7,22 +8,34 @@ namespace ShiftLogger.Application.Shifts.Requests.GetShiftsByEmployeeId;
 public class GetShiftsByEmployeeIdHandler
 {
     private readonly IShiftsRepository _shiftsRepository;
+    private readonly IEmployeeRepository _employeeRepository;
 
-    public GetShiftsByEmployeeIdHandler(IShiftsRepository shiftsRepository)
+    public GetShiftsByEmployeeIdHandler(IShiftsRepository shiftsRepository, IEmployeeRepository employeeRepository)
     {
         _shiftsRepository = shiftsRepository;
+        _employeeRepository = employeeRepository;
     }
 
-    public async Task<List<ShiftDto>> HandleAsync(GetShiftsQuery request, CancellationToken ct)
+    public async Task<Result<List<ShiftDto>?>> HandleAsync(GetShiftsQuery request)
     {
-        var shifts = await _shiftsRepository.GetAllShiftsByUserIdAsync(request.Id);
+        var employeeExistsResult = await _employeeRepository.EmployeeExistsById(request.Id);
+        if (!employeeExistsResult.Value)
+            return Result<List<ShiftDto>?>.Failure(employeeExistsResult.Errors);
 
-        return MapShifts(shifts);
+        var result = await _shiftsRepository.GetAllShiftsByUserIdAsync(request.Id);
+
+        if (result.IsFailure)
+            return Result<List<ShiftDto>?>.Failure(result.Errors);
+
+        return Result<List<ShiftDto>?>.Success(MapShifts(result.Value));
     }
 
-    private List<ShiftDto> MapShifts(List<Shift> shifts)
+    private List<ShiftDto> MapShifts(List<Shift>? shifts)
     {
         var output = new List<ShiftDto>();
+
+        if (shifts is null)
+            return output;
 
         foreach (var shift in shifts)
         {
