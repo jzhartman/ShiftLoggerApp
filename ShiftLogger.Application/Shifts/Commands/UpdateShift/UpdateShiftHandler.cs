938fc8db2b561e9a2d3faaee0ad5682360b4feca
jzhartman <jzhartman@gmail.com>
2026-08-12 22:58:16 -0400

    UpdateShiftHandler implements Results pattern

--- ShiftLogger.Application/Shifts/Commands/UpdateShift/UpdateShiftHandler.cs
+++ ShiftLogger.Application/Shifts/Commands/UpdateShift/UpdateShiftHandler.cs
@@ -1,4 +1,6 @@
 ﻿using ShiftLogger.Domain.Models;
+using ShiftLogger.Domain.Validation;
+using ShiftLogger.Domain.Validation.Errors;
 using ShiftLogger.Infrastructure.Repositories;
 
 namespace ShiftLogger.Application.Shifts.Commands.UpdateShift;
@@ -6,22 +8,42 @@ namespace ShiftLogger.Application.Shifts.Commands.UpdateShift;
 public class UpdateShiftHandler
 {
     private readonly IShiftsRepository _shiftsRepository;
+    private readonly IEmployeeRepository _employeeRepository;
 
-    public UpdateShiftHandler(IShiftsRepository shiftsRepository)
+    public UpdateShiftHandler(IShiftsRepository shiftsRepository, IEmployeeRepository employeeRepository)
     {
         _shiftsRepository = shiftsRepository;
+        _employeeRepository = employeeRepository;
     }
 
-    public async Task HandleAsync(UpdateShiftCommand shift)
+    public async Task<Result> HandleAsync(UpdateShiftCommand shift)
     {
-        await _shiftsRepository.UpdateShiftByIdAsync(new Shift
+        var updatedShift = new Shift
         {
             Id = shift.Id,
             EmployeeId = shift.EmployeeId,
             ClockInTime = shift.ClockInTime,
             ClockOutTime = shift.ClockInTime
-        });
+        };
 
-        await _shiftsRepository.SaveChangesAsync();
+        if (!(await _shiftsRepository.ShiftExistsById(updatedShift.Id)).Value)
+            return Result.Failure(Errors.ShiftIdNotFound);
+
+        var employeeExistsResult = await _employeeRepository.EmployeeExistsById(updatedShift.EmployeeId);
+        if (!employeeExistsResult.Value)
+            return Result.Failure(employeeExistsResult.Errors);
+
+        if (updatedShift.ClockInTime > updatedShift.ClockOutTime)
+            return Result.Failure(Errors.ClockInTimePrecedesClockOutTime);
+
+        if ((await _shiftsRepository.OverlapsExistingShiftsExcludingCurrent(updatedShift)).Value)
+            return Result.Failure(Errors.NewShiftOverlapsExistingShift);
+
+        var updateResult = await _shiftsRepository.UpdateShiftByIdAsync(updatedShift);
+
+        if (updateResult.IsFailure)
+            return updateResult;
+
+        return await _shiftsRepository.SaveChangesAsync();
     }
 }
