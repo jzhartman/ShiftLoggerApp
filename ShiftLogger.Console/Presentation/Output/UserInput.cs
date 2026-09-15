87f6ac52ca0fed06c47d2111e09b9acb9065b54a
jzhartman <jzhartman@gmail.com>
2026-09-14 22:45:12 -0400

    Deleted unused commented out code block

--- ShiftLogger.Console/Presentation/Output/UserInput.cs
+++ ShiftLogger.Console/Presentation/Output/UserInput.cs
@@ -96,18 +96,4 @@ internal static class UserInput
     {
         return AnsiConsole.Confirm(message);
     }
-
-    //internal bool GetEditContactConfirmationFromUser(FullContactViewModel originalContact, EditContactViewModel newContact)
-    //{
-    //    string preamble = $"Confirm the following changes for the contact {originalContact.FirstName} {originalContact.LastName}:";
-    //    string changes = string.Empty;
-
-    //    if (newContact.ChangedFirstName) changes += $"\t[yellow]{originalContact.FirstName}[/] to [green]{newContact.FirstName}[/]\r\n";
-    //    if (newContact.ChangedLastName) changes += $"\t[yellow]{originalContact.LastName}[/] to [green]{newContact.LastName}[/]\r\n";
-    //    if (newContact.ChangedPhoneNumber) changes += $"\t[yellow]{originalContact.PhoneNumber}[/] to [green]{newContact.PhoneNumber}[/]\r\n";
-    //    if (newContact.ChangedEmail) changes += $"\t[yellow]{originalContact.Email}[/] to [green]{newContact.Email}[/]\r\n";
-    //    if (newContact.ChangedCategory) changes += $"\t[yellow]{originalContact.CategoryName}[/] to [green]{newContact.CategoryName}[/]\r\n";
-
-    //    return AnsiConsole.Confirm($"{preamble}\r\n\r\n{changes}\r\nConfirm:");
-    //}
 }
